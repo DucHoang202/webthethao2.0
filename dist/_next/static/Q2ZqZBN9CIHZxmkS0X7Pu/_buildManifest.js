b6233fcf566661208410006ff0dad7e73e177a19
DucHoang202 <tomhoang152@gmail.com>
2025-11-26 13:06:20 +0700

    build

---
+++ dist/_next/static/Q2ZqZBN9CIHZxmkS0X7Pu/_buildManifest.js
@@ -0,0 +1,26 @@
+self.__BUILD_MANIFEST = {
+  "/Footer": [
+    "static/chunks/cb3c048cc7233e85.js"
+  ],
+  "/Header": [
+    "static/chunks/39272e4f05344bf3.js"
+  ],
+  "/NotFound": [
+    "static/chunks/09e9386b7b271e83.js"
+  ],
+  "/_error": [
+    "static/chunks/6b2f2383198c8303.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/Footer",
+    "/Header",
+    "/NotFound",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ dist/_next/static/chunks/8ce9f7aeff926b05.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/1d4e863ce7409b41.js",
+  "static/chunks/18141ccf4ffa35cf.js",
+  "static/chunks/turbopack-d9fb82320554526e.js"
+])
