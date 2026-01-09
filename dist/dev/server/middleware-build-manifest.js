046ba557db6e0d643398cf83e91e58a63ad29ef0
huynhman <manhuynh122@gmail.com>
2026-01-09 11:06:42 +0700

    fix

--- dist/dev/server/middleware-build-manifest.js
+++ dist/dev/server/middleware-build-manifest.js
@@ -4,20 +4,20 @@ globalThis.__BUILD_MANIFEST = {
   },
   "devFiles": [],
   "polyfillFiles": [
-    "static/chunks/ca6b9_next_dist_build_polyfills_polyfill-nomodule.js"
+    "static/chunks/node_modules_next_dist_build_polyfills_polyfill-nomodule.js"
   ],
   "lowPriorityFiles": [],
   "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_c9ea7942._.js",
-    "static/chunks/ca6b9_next_dist_compiled_react-dom_1b791b74._.js",
-    "static/chunks/ca6b9_next_dist_compiled_react-server-dom-turbopack_d1a27e4b._.js",
-    "static/chunks/ca6b9_next_dist_compiled_next-devtools_index_22fb73e3.js",
-    "static/chunks/ca6b9_next_dist_compiled_bce8dbb1._.js",
-    "static/chunks/ca6b9_next_dist_client_706205d3._.js",
-    "static/chunks/ca6b9_next_dist_1c8f7088._.js",
-    "static/chunks/ca6b9_@swc_helpers_cjs_c210b7cc._.js",
-    "static/chunks/webthethao2_0-main_webthethao2_0-main_a0ff3932._.js",
-    "static/chunks/turbopack-webthethao2_0-main_webthethao2_0-main_29bd2701._.js"
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_956a0d3a._.js",
+    "static/chunks/node_modules_next_dist_compiled_react-dom_1e674e59._.js",
+    "static/chunks/node_modules_next_dist_compiled_react-server-dom-turbopack_9212ccad._.js",
+    "static/chunks/node_modules_next_dist_compiled_next-devtools_index_1dd7fb59.js",
+    "static/chunks/node_modules_next_dist_compiled_a0e4c7b4._.js",
+    "static/chunks/node_modules_next_dist_client_17643121._.js",
+    "static/chunks/node_modules_next_dist_f3530cac._.js",
+    "static/chunks/node_modules_@swc_helpers_cjs_d80fb378._.js",
+    "static/chunks/_a0ff3932._.js",
+    "static/chunks/turbopack-_23a915ee._.js"
   ]
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
